41e7fb59655564f9570ea0320562fb554800cb96
ntdunkley <n.dunkley8@gmail.com>
2016-07-04 15:07:24 +0200

    Updated Pollers

--- Backend/src/main/java/poller/GmailPoller.java
+++ Backend/src/main/java/poller/GmailPoller.java
@@ -3,8 +3,12 @@ package poller;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class GmailPoller implements Poller {
+	private ConcurrentLinkedQueue queue;
+	private String userId;
+	
 	public GmailPoller(ConcurrentLinkedQueue queue, String userId) {
-
+		this.queue = queue;
+		this.userId = userId;
 	}
 
 	public void run() {
--- Backend/src/main/java/poller/Poller.java
+++ Backend/src/main/java/poller/Poller.java
@@ -1,4 +1,8 @@
 package poller;
 
+import data.*;
+
 public interface Poller extends Runnable {
+	public RawData poll();
+	public void addToQueue(RawData data);
 }
