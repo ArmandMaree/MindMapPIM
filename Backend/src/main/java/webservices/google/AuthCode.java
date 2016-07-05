4b78123d584c38f3eb810021aeb18ebf4aa3f7a3
Armand Maree <aamaree@gmail.com>
2016-07-05 22:38:49 +0200

    - Server now waits for POST requests with the Google Auth Code

---
+++ Backend/src/main/java/webservices/google/AuthCode.java
@@ -0,0 +1,17 @@
+package webservices.google;
+
+public class AuthCode {
+    private String authCode;
+
+    public AuthCode() {
+    	
+    }
+
+    public AuthCode(String authCode) {
+    	this.authCode = authCode;
+    }
+
+    public String getAuthCode() {
+        return authCode;
+    }
+}
