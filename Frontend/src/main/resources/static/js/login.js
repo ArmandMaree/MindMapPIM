0bb9bc3ea0ed0f524d0c107b746af235fa5ed679
ArnoGrobler <arnogrobler@hotmail.com>
2016-07-12 09:08:17 +0200

    Fixed google login button and made buttons look better

--- Frontend/src/main/resources/static/js/login.js
+++ Frontend/src/main/resources/static/js/login.js
@@ -1,4 +1,133 @@
+var auth2; // The Sign-In object.
+var googleUser; // The current user.
+
+var startApp = function() {
+  gapi.load('auth2', initSigninV2);
+};
+
+var initSigninV2 = function() {
+  auth2 = gapi.auth2.init({
+      client_id: '570253498384-r14raqpo4lcqpjggmp05h6359dm6ogfo.apps.googleusercontent.com',
+      scope: 'profile'
+  });
+   auth2.attachClickHandler('googleLogin', {}, onSuccess, onFailure);
+  auth2.isSignedIn.listen(signinChanged);
+  // auth2.currentUser.listen(userChanged);
+  if (auth2.isSignedIn.get() == true) {
+    auth2.signIn();
+  }
+  refreshValues();
+};
+
+var signinChanged = function (val) {
+  console.log('Signin state changed to ', val);
+  if(val === true){
+     $("#googleLogin").animate({
+          top: '100px',
+          opacity: '0.0'
+
+      });            
+      $("#facebookLogin").animate({
+          top: '100px',
+          opacity: '0.0'
+
+      });
+      $("#tos").animate({
+          top: '100px',
+          opacity: '0.0'
+
+      });
+      $("#web").animate({
+          top: '100px',
+          opacity: '0.0'
+
+      });
+
+      $("#avatar").delay("slow").animate({
+          top: '70px',
+          opacity: '0.3'
+
+      });
+      $("#welcome").show();
+      $("#welcome").delay(1000).animate({
+          opacity: '1'
+
+      });
+      $('#avatar').fadeOut(0, function() {
+          $('#avatar').fadeIn(0);
+          $('#avatar').css("background","#eee url('/images/avatar3.png')");
+           $('#avatar').css("background-size","cover");
+            $('#avatar').css("opacity","1");
+      });
+      $("#continue").show();
+      $("#continue").delay(2000).animate({
+          opacity: '1'
+      });
+
+    // }
+    
+    
+  }
+  // document.getElementById('signed-in-cell').innerText = val;
+};
+
+// var userChanged = function (user) {
+//   console.log('User now: ', user);
+//   googleUser = user;
+//   updateGoogleUser();
+//   document.getElementById('curr-user-cell').innerText =
+//     JSON.stringify(user, undefined, 2);
+// };
+
+// var updateGoogleUser = function () {
+//   if (googleUser) {
+//     document.getElementById('user-id').innerText = googleUser.getId();
+//     document.getElementById('user-scopes').innerText =
+//       googleUser.getGrantedScopes();
+//     document.getElementById('auth-response').innerText =
+//       JSON.stringify(googleUser.getAuthResponse(), undefined, 2);
+//   } else {
+//     document.getElementById('user-id').innerText = '--';
+//     document.getElementById('user-scopes').innerText = '--';
+//     document.getElementById('auth-response').innerText = '--';
+//   }
+// };
+var refreshValues = function() {
+  if (auth2){
+    console.log('Refreshing values...');
+
+    googleUser = auth2.currentUser.get();
+
+    console.log(JSON.stringify(googleUser, undefined, 2));
+    console.log(auth2.isSignedIn.get());
+
+    // updateGoogleUser();
+  }
+}
+
+  // function attachSignin(element) {
+  //   console.log(element.id);
+  //   auth2.attachClickHandler(element, {},
+  //       function(googleUser) {
+  //         console.log("Signed in: " +
+  //             googleUser.getBasicProfile().getName());
+  //       }, function(error) {
+  //         alert(JSON.stringify(error, undefined, 2));
+  //       });
+  // }
+  var onSuccess = function(user) {
+    console.log('Signed in as ' + user.getBasicProfile().getName());
+ };
+
+/**
+ * Handle sign-in failures.
+ */
+var onFailure = function(error) {
+    console.log(error);
+};
+
         function googleretrieve(){
+          
           var auth2 = gapi.auth2.getAuthInstance();
           auth2.grantOfflineAccess({'redirect_uri': 'postmessage'}).then(signInCallback);
         
