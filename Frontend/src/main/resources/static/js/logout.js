5188b19c3bc0c15cfcf23b8c873df6d0f2f286ad
LochnerAmy <u14038600@tuks.co.za>
2016-07-20 18:45:33 +0200

    -Added comments to javascript function

--- Frontend/src/main/resources/static/js/logout.js
+++ Frontend/src/main/resources/static/js/logout.js
@@ -36,24 +36,34 @@ function refreshValues() {
     console.log(auth2.isSignedIn.get());
   }
 }
-
-	(function(d, s, id) {
+/**
+*	This loads the Facebook SDK asynchronously
+*/
+(function(d, s, id) 
+{
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-	window.fbAsyncInit = function() {
-	  FB.init({
-	    appId      : '1051696778242173',
-	    cookie     : true,  // enable cookies to allow the server to access 
-	                        // the session
-	    xfbml      : true,  // parse social plugins on this page
-	    version    : 'v2.5' // use graph api version 2.5
-	  });
-	}
-
+    fjs.parentNode.insertBefore(js, fjs); 
+}(document, 'script', 'facebook-jssdk'));
+	
+/** 
+*	This function initialises the JavaScript SDK
+*/
+window.fbAsyncInit = function() 
+{
+    FB.init({
+    appId      : '1051696778242173',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.5' // use graph api version 2.5
+  });
+}
+/**
+*	This function is called when a user clicks on the logout button. It determines which social platform the user logged in with.
+*/
 function logout()
 {
     startApp();
