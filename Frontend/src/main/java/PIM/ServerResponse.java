32fba01b93deb055535ab66c76d75fc3a77149cb
Arno Grobler <arnogrobler@hotmail.com>
2016-08-12 07:15:15 +0200

    - Added boolean to server response object to see if registered

--- Frontend/src/main/java/PIM/ServerResponse.java
+++ Frontend/src/main/java/PIM/ServerResponse.java
@@ -5,14 +5,24 @@ import java.io.Serializable;
 public class ServerResponse implements Serializable {
 
     private String content;
+    private Boolean isRegistered;
 
     public ServerResponse(String content) {
         this.content = content;
         System.out.println("Resonse created");
     }
+    public ServerResponse(Boolean isRegistered) {
+        this.isRegistered = isRegistered;
+        System.out.println("Response created");
+    }
 
     public String getContent() {
         return content;
     }
+
+    public String getIsRegistered() {
+        return isRegistered;
+    }
+
 
 }
