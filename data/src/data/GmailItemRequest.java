f59d1525b655e63b633a5a54b658001f5a82082b
ArnoGrobler <arnogrobler@hotmail.com>
2016-08-13 14:42:02 +0200

    - Added new extended class for making sidebar update requests for gmail

---
+++ data/src/data/GmailItemRequest.java
@@ -0,0 +1,26 @@
+package data;
+
+import java.io.Serializable;
+
+/**
+* A response for new topics based on a TopicRequest.
+*
+* @author  Arno Grobler
+* @since   2016-08-13
+* @see GmailItemRequest
+*/
+
+public class GmailItemRequest extends ItemRequest {
+	/**
+	* Default empty GmailItemRequest constructor
+	*/
+	public GmailItemRequest() {
+		super();
+	}
+	/**
+	* GmailItemRequest constructor given by te user
+	*/
+	public GmailItemRequest(String[] itemIds, String userId) {
+		super(itemIds,userId);
+	}
+}
