e33aeb7a85799dedbb06c682f3bd6ac7923cb7dc
ArnoGrobler <arnogrobler@hotmail.com>
2016-08-18 14:06:18 +0200

    - Added windows bats for uploading

--- data/winupload.bat
+++ data/winupload.bat
@@ -1,1 +1,2 @@
 winscp /command "option batch abort" "option confirm off" "open sftp://armand:maichich7ooD@bubbles.iminsys.com/" "put data.jar /home/armand/gradle/data/" "exit"
+if "%1"=="doexit" exit
