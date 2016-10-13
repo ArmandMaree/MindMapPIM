6f79ab9328ed34152d437c9d74c73fdb89fb16ef
LochnerAmy <u14038600@tuks.co.za>
2016-10-13 16:55:48 +0200

    -Sanitized twitter input

--- Frontend/src/main/resources/static/js/twitter.js
+++ Frontend/src/main/resources/static/js/twitter.js
@@ -2,8 +2,13 @@ function saveTwitterDetails()
 {
 	var twitUser = $("#TwitUser").val();
 	console.log("TwitUser: "+ twitUser);
-	document.cookie = "twitter="+ twitUser;
-	localStorage.setItem("twitterUser", twitUser);
+	if(twitUser[0] == "@")
+	{
+
+		twitUser = twitUser.substring(1,twitUser.length);
+	}
+	document.cookie = "twitter="+ escape(twitUser);
+	localStorage.setItem("twitterUser", escape(twitUser));
 	// Retrieve
 	window.close();
 	// alert(JSON.stringify(authCodes));
